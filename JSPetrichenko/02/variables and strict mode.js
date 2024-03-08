10952ddcd7b953b50977787932921f685d048c8b
gavrilovaei <gavrilova-e@yandex.ru>
2024-03-08 23:48:48 +0300

    first commit

---
+++ JSPetrichenko/02/variables and strict mode.js
@@ -0,0 +1,13 @@
+"use strict";
+let number = 5;
+const leftBorderWidth = 1;
+number = 10;
+console.log(number);
+// leftBorderWidth = 10; // ошибка -константы изменять нельзя
+console.log(leftBorderWidth);
+
+// a = 15;
+// console.log(a); // ошибка в "use strict" нельзя объявить переменную без ключевого слова
+
+alert(5);
+[].push('a');
